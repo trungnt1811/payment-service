3486c93b7415d592bb56005dc77253d5a2c92421
trung.nguyen <trung.nguyen@genefriendway.com>
2024-10-09 11:07:24 +0700

    refactor: remove unused modules

--- onchain-handler/internal/dto/response.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type LockEventDTOResponse struct {
-	NextPage int            `json:"next_page"`
-	Page     int            `json:"page"`
-	Size     int            `json:"size"`
-	Total    int64          `json:"total,omitempty"`
-	Data     []LockEventDTO `json:"data"`
-}
--- onchain-handler/internal/route/v1.go
+++ onchain-handler/internal/route/v1.go
@@ -8,72 +8,17 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
 
-	"github.com/genefriendway/onchain-handler/blockchain/listeners"
 	"github.com/genefriendway/onchain-handler/conf"
-	"github.com/genefriendway/onchain-handler/internal/module/blockstate"
-	"github.com/genefriendway/onchain-handler/internal/module/lock"
-	"github.com/genefriendway/onchain-handler/internal/module/membership"
 	"github.com/genefriendway/onchain-handler/internal/module/transfer"
-	"github.com/genefriendway/onchain-handler/internal/utils/log"
 )
 
 func RegisterRoutes(r *gin.Engine, config *conf.Configuration, db *gorm.DB, ethClient *ethclient.Client, ctx context.Context) {
 	v1 := r.Group("/api/v1")
 	appRouter := v1.Group("")
 
-	// SECTION: reward tokens
+	// SECTION: tokens transfer
 	transferRepository := transfer.NewTransferRepository(db)
 	transferUCase := transfer.NewTransferUCase(transferRepository, ethClient, config)
 	transferHandler := transfer.NewTransferHandler(transferUCase)
 	appRouter.POST("/transfer", transferHandler.Transfer)
-
-	// SECTION: membership purchase
-	membershipRepository := membership.NewMembershipRepository(db)
-	membershipUCase := membership.NewMembershipUCase(membershipRepository)
-	membershipHandler := membership.NewMembershipHandler(membershipUCase)
-	appRouter.GET("/membership/events", membershipHandler.GetMembershipEventsByOrderIDs)
-
-	// SECTION: lock history
-	lockRepository := lock.NewLockRepository(db)
-	lockUCase := lock.NewLockUCase(lockRepository)
-	lockHandler := lock.NewLockHandler(lockUCase)
-	appRouter.GET("/lock/latest-events", lockHandler.GetLatestLockEventsByUserAddress)
-
-	// SECTION: events listener
-	// base event listener for common listener logic
-	baseEventListener := listeners.NewBaseEventListener(
-		ethClient,
-		blockstate.NewBlockstateRepository(db),
-		&config.Blockchain.StartBlockListener,
-	)
-	// membership event listener
-	membershipEventListener, err := listeners.NewMembershipEventListener(
-		baseEventListener,
-		ethClient,
-		config.Blockchain.MembershipContractAddress,
-		membershipRepository,
-	)
-	if err != nil {
-		log.LG.Errorf("Failed to initialize MembershipEventListener: %v", err)
-		return
-	}
-	membershipEventListener.Register(ctx)
-	// lock event listener
-	lockEventListener, err := listeners.NewLockEventListener(
-		baseEventListener,
-		ethClient,
-		config.Blockchain.LockContractAddress,
-		lockRepository,
-	)
-	if err != nil {
-		log.LG.Errorf("Failed to initialize LockEventListener: %v", err)
-		return
-	}
-	lockEventListener.Register(ctx)
-
-	go func() {
-		if err := baseEventListener.RunListener(ctx); err != nil {
-			log.LG.Errorf("Error running event listeners: %v", err)
-		}
-	}()
 }
