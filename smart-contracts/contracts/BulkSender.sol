888e32b69eacf59c36211af9d47eebe44160c301
trung.nguyen <trung.nguyen@genefriendway.com>
2024-10-09 19:09:25 +0700

    refactor: change BulkSender smartcontract

--- smart-contracts/contracts/BulkSender.sol
+++ smart-contracts/contracts/BulkSender.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BulkSender is Ownable {
+    using SafeERC20 for IERC20;
+
     event BulkTransfer(address indexed token, uint256 totalAmount, uint256 recipientCount);
 
     // This function allows sending AVAX to the contract
@@ -25,14 +28,17 @@ contract BulkSender is Ownable {
     function _validateInputs(address[] calldata recipients, uint256[] calldata values) internal pure {
         require(recipients.length == values.length, "Recipients and values length mismatch");
         require(recipients.length > 0, "No recipients");
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(recipients[i] != address(0), "Cannot transfer to zero address");
+        }
     }
 
     // Internal function to process the actual transfers
     function _processTransfers(IERC20 erc20, address[] calldata recipients, uint256[] calldata values) internal returns (uint256 totalAmount) {
         for (uint256 i = 0; i < recipients.length; i++) {
             // Ensure that the transfer is successful
-            bool success = erc20.transferFrom(msg.sender, recipients[i], values[i]);
-            require(success, "Transfer failed"); // Revert if transfer fails
+            erc20.safeTransferFrom(msg.sender, recipients[i], values[i]);
             totalAmount += values[i];
         }
     }
